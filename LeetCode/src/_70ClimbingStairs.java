ae0642ba7abcd243aaabf4b567e1d6e1e7028a1e
oliver <oliverguo29@gmail.com>
2019-08-23 00:12:55 +0100

    70

---
+++ LeetCode/src/_70ClimbingStairs.java
@@ -0,0 +1,15 @@
+class _70ClimbingStairs {
+    public int climbStairs(int n) {
+        int [] res =new int[n+1];
+        res[0]=1;
+        res[1]=1;
+        for(int i=2;i<=n;i++){
+            res[i]=res[i-1]+res[i-2];
+        }
+
+        return res[n];
+    }
+
+
+
+}
