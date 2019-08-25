09b4dce340cdbd37f5cb2d017ca2b0450249067c
oliver <oliverguo29@gmail.com>
2019-08-25 11:43:47 +0100

    167

---
+++ LeetCode/src/_167TwoSumII_Inputarrayissorted.java
@@ -0,0 +1,19 @@
+class _167TwoSumII_Inputarrayissorted {
+    //o n 2 时间复杂度
+    public int[] twoSum(int[] numbers, int target) {
+        int [] result = new int [2];
+
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                if(numbers[i]+numbers[j]==target){
+                    result[0]=i;
+                    result[1]=j;
+                }
+                return result;
+            }
+        }
+
+
+        return  null;
+    }
+}
