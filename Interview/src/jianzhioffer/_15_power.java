5a99690ee82920b44ba499bebc2a3f36c9d2d710
oliver <oliverguo29@gmail.com>
2019-09-10 22:48:14 +0100

    jianzhioffer day3

--- Interview/src/jianzhioffer/_15_power.java
+++ Interview/src/jianzhioffer/_15_power.java
@@ -2,10 +2,40 @@ package jianzhioffer;
 
 /**
  * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ *               n/2
+ *          (x*x)           n%2==0
+ *  n
+ * x   =           n/2
+ *          x*(x*x)         n%2==1
  */
 public class _15_power {
     public double Power(double base, int exponent) {
-        double pow = Math.pow(base, exponent);
-        return pow;
+        if(exponent==0){    //0次方
+            return 1;
+        }
+
+        if(exponent==1){
+            return base;
+        }
+
+        boolean flag =false;
+        if(exponent<0){
+            exponent=-exponent;
+            flag=true;
+        }
+
+        double pow=Power(base*base,exponent/2);
+        if(exponent%2==0){
+            pow=pow*base;       //不等于0      乘奇数次
+        }
+
+        return flag?(1/pow):pow;
     }
 }
+
+
+
+/**
+ double pow = Math.pow(base, exponent);
+ return pow;
+ **/
