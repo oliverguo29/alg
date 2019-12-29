c274c025058edd20595141201f20500379839296
oliver <oliverguo29@gmail.com>
2019-12-29 17:30:18 +0800

    sort

--- LeetCode/src/_215KthLargestElementinanArray.java
+++ LeetCode/src/_215KthLargestElementinanArray.java
@@ -3,12 +3,6 @@ import java.util.Arrays;
 class _215KthLargestElementinanArray {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
-        int result=0;
-        int length=nums.length-1;
-        for(int i=0;i<k;i++){
-               result= nums[length--];
-        }
-
-        return  result;
+        return nums[nums.length-k];
     }
 }
--- sort/src/insert/SimpleInsert.java
+++ sort/src/insert/SimpleInsert.java
@@ -4,9 +4,8 @@ public class SimpleInsert {
     //简单插入  给定数组【2 1 7 9 5 8】从左到右 从小到大排序
 
     public static void simpleInsert(int [] arr){
-       for(int i=1;i<arr.length;i++){
-           int cur=arr[i];
-           int j;
+       for(int i=1,j,cur;i<arr.length;i++){
+            cur = arr[i];
            for(j=i-1;j>=0&&arr[j]>cur;j--){
                arr[j+1]=arr[j];
            }
